146589b3ee6808bb6300e924152902701a87768d
Ben Miles <155984943+benmiles-hashi@users.noreply.github.com>
2024-06-28 15:24:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "myec2" {
   ami                     = var.ami
-  instance_type           = "m5.xlarge"
+  instance_type           = "t2.micro"
 
   tags = {
     Name = "MyEC2-VCS-Prod"
     owner = "Ben Miles"
+    Env   = "Prod"
   }
 
 }
